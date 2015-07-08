63606731c9d654f1842135b11aa8a46f4d46731e
Shihan Ai <g3aishih@gmail.com>
2015-07-07 23:18:18 -0400

    Indexes each course in CSC

--- scraper.py
+++ scraper.py
@@ -5,7 +5,7 @@ client = MongoClient('localhost', 27017)
 r = requests.get('http://coursefinder.utoronto.ca/course-search/search/courseSearch?viewId=CourseSearch-FormView&methodToCall=start')
 params = {'divId':'ARTSC', 'deptId':'CSC'}
 headers = {'Cookie':'kualiSessionId='+str(r.cookies['kualiSessionId'])+';JSESSIONID='+str(r.cookies['JSESSIONID'])}
-response = requests.get('http://coursefinder.utoronto.ca/course-search/search/courseSearch/course/browseSearch', params=params, headers=headers)
+res = requests.get('http://coursefinder.utoronto.ca/course-search/search/courseSearch/course/browseSearch', params=params, headers=headers)
 
-print(response.status_code)
-
+print('Status Code: ' + str(res.status_code))
+print(res.json()['aaData'][0])
