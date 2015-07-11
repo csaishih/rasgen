0e778816e6234b1f86f60442ca6540d6c11e7ca5
Shihan Ai <g3aishih@gmail.com>
2015-07-11 02:43:17 -0400

    Separated code

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # RaS Generator
+**RaS Generator** is a tool that generates class schedules for University of Toronto students. The goal is to provide a simpler solution to course planning.
+
+## Current Version
+1.0.0 (Alpha)
+
+## Installation
+
+## How to use - Scraper
+Go to the directory with `scraper.py` and run:
+```bash
+$python scraper.py
+```
+If `courses.dat` does not exist in your working directory it will be created for you.
+To update `courses.dat` with the most current data from [coursefinder.utoronto.ca](coursefinder.utoronto.ca)
--- scraper.py
+++ scraper.py
@@ -1,87 +1,25 @@
-import requests
+import os
 import cPickle
-from lxml import html
+import argparse
 from pymongo import MongoClient
-from Department import Department
-from Course import Course
 
-#define constants
-JSON_OBJECT_NAME = "aaData"
-OFFSET_TRIM_FRONT = 72
-OFFSET_TRIM_BACK = 21
-OFFSET_COURSE_LINK = 27
-OFFSET_LINK_LENGTH = 14
-
-def parseElements(element):
-	starting_index = element.value.index('searchForCourseByDept') + OFFSET_TRIM_FRONT
-	ending_index = len(element.value) - OFFSET_TRIM_BACK
-	dept_data = element.value[starting_index : ending_index]
-	dept_data = dept_data.replace(',', '')
-	dept_data = dept_data.replace("' '", "|")
-	dept_data = dept_data.replace("''", "|")
-	return dept_data.split('|')
-
-def getHeader(res):
-	return {'Cookie':'kualiSessionId='+str(res.cookies['kualiSessionId'])+';JSESSIONID='+str(res.cookies['JSESSIONID'])}
-
-def getElements(res):
-	element = html.fromstring(res.text)
-	return element.xpath('//input[@name="script"]')
-
-def getDepartments(elements):
-	dept_list = []
-	for i in range(0, len(elements)):
-			if 'searchForCourseByDept' in elements[i].value:
-				parsed_elements = parseElements(elements[i])
-				dept_list.append(Department(parsed_elements[0], parsed_elements[1], parsed_elements[2], parsed_elements[3]))
-	return dept_list
-
-def getParams(departments):
-	params = []
-	for i in range (0, len(departments)):
-		p = {'deptId': departments[i].code.strip(), 'divId': departments[i].faculty_code.strip()}	
-		params.append(p)
-	return params
-
-def getCourse(headers, param):
-	res = requests.get('http://coursefinder.utoronto.ca/course-search/search/courseSearch/course/browseSearch', params=param, headers=headers)
-	try:
-		courses = res.json()[JSON_OBJECT_NAME]
-		course_list = []
-		for i in range(0, len(courses)):
-			course = courses[i]
-			starting_index = course[1].index('courseSearch/coursedetails/') + OFFSET_COURSE_LINK
-			ending_index = starting_index + OFFSET_LINK_LENGTH
-
-			link = course[1][starting_index : ending_index]
-			code = link[:8]
-			title = course[2].encode('ascii', 'ignore')
-			credits = course[3]
-			campus = course[4]
-			dept = course[5]
-			year = course[6].split(' ')[0]
-			semester = course[6].split(' ')[1]
-			semester_code = link[8]
-			faculty = course[7]
-
-			course_list.append(Course(link, code, title, credits, campus, dept, year, semester, semester_code, faculty))
-		return course_list
-
-	except ValueError:
-		return []
-
-def getData():	
-	res = requests.get('http://coursefinder.utoronto.ca/course-search/search/courseSearch?viewId=CourseSearch-FormView&methodToCall=start')
-	headers = getHeader(res)
-	elements = getElements(res)
-	departments = getDepartments(elements)
-	params = getParams(departments)
-	courses = []
-	for i in range(0, len(params)):
-		courses += getCourse(headers, params[i])
-	cPickle.dump(courses, open('data.dat', 'wb'))
-	
+parser = argparse.ArgumentParser()
+parser.add_argument('-u', dest='flag', nargs='?', help='Generate/Update courses.dat', const='u', default='')
+args = parser.parse_args()
 
 if __name__ == '__main__':
-	getData()
-	client = MongoClient('localhost', 27017)
+	if (args.flag == 'u' or not os.path.isfile('courses.dat')):
+		print('Generating courses.dat ...')
+		try:
+			os.system('python gen_data.py')
+			print('OK\n')
+		except Exception:
+			print('FAIL\n')
+
+	try:
+		courses = cPickle.load(open('courses.dat', 'rb'))
+		client = MongoClient('localhost', 27017)
+		#Insert courses into MongoDB
+
+	except IOError:
+		print("Error: Cannot find courses.dat\nPlease run 'python scraper.py -u'")
