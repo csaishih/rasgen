bb7c6f08ee127653c62a1d8baad15833969d22e4
Shihan Ai <g3aishih@gmail.com>
2015-07-10 21:03:18 -0400

    Can now retrieve all courses

--- scraper.py
+++ scraper.py
@@ -10,33 +10,39 @@ REMOVE_RETURN_FALSE = 21
 def getHeader(res):
 	return {'Cookie':'kualiSessionId='+str(res.cookies['kualiSessionId'])+';JSESSIONID='+str(res.cookies['JSESSIONID'])}
 
-def getTree(res):
-	tree = html.fromstring(res.text)
-	return tree.xpath('//input[@name="script"]')
+def getElements(res):
+	element = html.fromstring(res.text)
+	return element.xpath('//input[@name="script"]')
+
+def getDepartments(elements):
+	dept_list = []
+	for i in range(0, len(elements)):
+			if 'searchForCourseByDept' in elements[i].value:
+				dept_data = (elements[i].value[elements[i].value.find('searchForCourseByDept') + REMOVE_SEARCH_FOR_COURSE_BY_DEPT:len(elements[i].value) - REMOVE_RETURN_FALSE]).replace(',', '').replace("' '", "|").replace("''", "|").split('|')
+				dept_list.append(Department(dept_data[0], dept_data[1], dept_data[2], dept_data[3]))
+	return dept_list
+
+def getParams(departments):
+	params = []
+	for i in range (0, len(departments)):
+		p = {'deptId': departments[i].code.strip(), 'divId': departments[i].faculty_code.strip()}	
+		params.append(p)
+	return params
+
+def getCourses(headers, param):
+	res = requests.get('http://coursefinder.utoronto.ca/course-search/search/courseSearch/course/browseSearch', params=param, headers=headers)
+	print(res.json())
 
 def getData():	
 	res = requests.get('http://coursefinder.utoronto.ca/course-search/search/courseSearch?viewId=CourseSearch-FormView&methodToCall=start')
 	headers = getHeader(res)
-	tree = getTree(res)
+	elements = getElements(res)
+	departments = getDepartments(elements)
+	params = getParams(departments)
 
-	dept_list = []
-	params = []
-	for i in range(0, len(tree)):
-		if 'searchForCourseByDept' in tree[i].value:
-			dept_data = (tree[i].value[tree[i].value.find('searchForCourseByDept') + REMOVE_SEARCH_FOR_COURSE_BY_DEPT:len(tree[i].value) - REMOVE_RETURN_FALSE]).replace(',', '').replace("' '", "|").replace("''", "|").split('|')
-			dept_list.append(Department(dept_data[0], dept_data[1], dept_data[2], dept_data[3]))
-			p = {}
-			p['divId'] = dept_data[0]
-			p['deptId'] = dept_data[2]
-			params.append(p)
-
-	
-
-	res = requests.get('http://coursefinder.utoronto.ca/course-search/search/courseSearch/course/browseSearch', params=params[0], headers=headers)
-	print(res.status_code)
-
-#print('Status Code: ' + str(res.status_code))
-#print(res.json()['aaData'][0])
+	#for i in range(0, len(params)):
+		#getCourses(headers, params[i])
+	getCourses(headers, params[18])
 
 if __name__ == '__main__':
 	getData()
